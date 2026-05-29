4f84ba298d9272271d25392f713d73d386b1f5b8
NoirAbhinav <abhinavbbps2000@gmail.com>
2026-05-29 12:20:07 +0530

    chore: updated tracking

--- backend/internal/app/workers.go
+++
@@ -1,8 +0,0 @@
-package app
-
-import "context"
-
-func startWorkers(ctx context.Context, deps *Dependencies) {
-	go deps.SyncWorker.Start(ctx)
-	go deps.SchedulerEngine.Start(ctx)
-}
--- oci-deploy/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "tenancy_ocid"     {}
-variable "user_ocid"        {}
-variable "fingerprint"      {}
-variable "private_key_path" {}
-variable "region"           { default = "ap-mumbai-1" }
-variable "compartment_ocid" {}
-variable "ssh_public_key"   {}
